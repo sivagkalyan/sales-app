15b41ef5695f165210f7b615226320d761d275ba
sivagkalyan <kalyansiva118.com>
2023-11-28 18:59:18 +0530

    Added course module

--- app.py
+++ app.py
@@ -1,25 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bootstrap import Bootstrap
 
 
 db = SQLAlchemy()
 app = Flask(__name__)
-Bootstrap(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 db.init_app(app)
 from models.user import User
 
 
 @app.route('/')
 def hello_world():
-    rec = db.get_or_404(User, 1)
-    #rec = User.query.get_or_404(1)
-    return render_template('index.html', user=rec)
-
-
-@app.route('/users')
-def users():
-    user_recs = db.session.query(User).all()
-    users = list(map(lambda rec: rec.__dict__, user_recs))
-    return render_template('users.html', users=users)
+    # rec = db.get_or_404(User, 1)
+    rec = User.query.get_or_404(1)
+    return f"<p>Hello, World! from {rec.username}</p>"
